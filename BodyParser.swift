3d94af611a907880a97908fee3ec54738ee3dc65
Daniel Firsht <dfirsht@us.ibm.com>
2015-12-16 10:44:43 -0600

    got router lib to compile

---
+++ BodyParser.swift
@@ -0,0 +1,115 @@
+//
+//  BodyParser.swift
+//  router
+//
+//  Created by Ira Rosen on 29/11/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+import SwiftyJSON
+import sys
+import net
+import io
+import Foundation
+
+public class BodyParser : RouterMiddleware {
+    private static let BUFFER_SIZE = 2000
+    
+    public init() {}
+    
+    public func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) {
+        request.body = BodyParser.parse(request, contentType: request.serverRequest.headers["Content-Type"])
+        next()
+    }
+    
+    public class func parse(message: Reader, contentType: String?) -> ParsedBody? {
+        if let contentType = contentType {
+            do {
+                if ContentType.isType(contentType, typeDescriptor: "json") {
+                    let bodyData = try readBodyData(message)
+                    let json = JSON(data: bodyData)
+                    if json != JSON.null {
+                        return ParsedBody(json: json)
+                    }
+                }
+                else if ContentType.isType(contentType, typeDescriptor: "urlencoded") {
+                    let bodyData = try readBodyData(message)
+                    var parsedBody = [String:String]()
+                    var success = true
+                    if let bodyAsString = String(data: bodyData, encoding: NSUTF8StringEncoding) {
+                        let bodyAsArray = bodyAsString.componentsSeparatedByString("&")
+                        for element in bodyAsArray {
+                            let elementPair = element.componentsSeparatedByString("=")
+                            if elementPair.count == 2 {
+                                parsedBody[elementPair[0]] = elementPair[1]
+                            }
+                            else {
+                                success = false
+                            }
+                        }
+                        if success && parsedBody.count > 0 {
+                            return ParsedBody(urlEncoded: parsedBody)
+                        }
+                    }
+                }
+                else if ContentType.isType(contentType, typeDescriptor: "text/*") {
+                    let bodyData = try readBodyData(message)
+                    if let bodyAsString = String(data: bodyData, encoding: NSUTF8StringEncoding) {
+                        return ParsedBody(text:  bodyAsString)
+                    }
+                }
+            }
+            catch  {
+                //response.error = Error()
+            }
+        }
+        
+        return nil
+    }
+    
+    private class func readBodyData(reader: Reader) throws -> NSMutableData {
+        var body = [UInt8](count: BodyParser.BUFFER_SIZE, repeatedValue: 0)
+        let bodyData = NSMutableData()
+        
+        var length = try reader.readBuffer(&body)
+        while length != 0 {
+            bodyData.appendBytes(body, length: length)
+            length = try reader.readBuffer(&body)
+        }
+        return bodyData
+    }
+    
+}
+
+
+public class ParsedBody {
+    
+    private var jsonBody: JSON?
+    private var urlEncodedBody: [String:String]?
+    private var textBody: String?
+    
+    public init (json: JSON) {
+        jsonBody = json
+    }
+    
+    public init (urlEncoded: [String:String]) {
+        urlEncodedBody = urlEncoded
+    }
+    
+    public init (text: String) {
+        textBody = text
+    }
+    
+    public func asJson() -> JSON? {
+        return jsonBody
+    }
+    
+    public func asUrlEncoded() -> [String:String]? {
+        return urlEncodedBody
+    }
+    
+    public func asText() -> String? {
+        return textBody
+    }
+}
+
---
+++ ContentType.swift
@@ -0,0 +1,100 @@
+//
+//  ContentType.swift
+//  router
+//
+//  Created by Ira Rosen on 29/11/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+public class ContentType {
+    
+    private static var extToContentType = [String:String]()
+        
+    public class func initialize () {
+        for (contentType, exts) in rawTypes {
+            for ext in exts {
+                extToContentType[ext] = contentType
+            }
+        }
+    }
+    
+    
+    public class func contentTypeForExtension (ext: String) -> String? {
+        return extToContentType[ext]
+    }
+    
+    
+    public class func isType (messageContentType: String, typeDescriptor: String) -> Bool {
+        let type = typeDescriptor.lowercaseString
+        let typeAndSubtype = messageContentType.componentsSeparatedByString(";")[0].lowercaseString
+        
+        if typeAndSubtype == type {
+            return true
+        }
+        
+        // typeDescriptor is file extension
+        if typeAndSubtype == extToContentType[type] {
+            return true
+        }
+        
+        // typeDescriptor is a shortcut
+        let normalizedType = normalizeType(type)
+        if typeAndSubtype == normalizedType {
+            return true
+        }
+        
+        // the types match and the subtype in typeDescriptor is "*"
+        let messageTypePair = typeAndSubtype.componentsSeparatedByString("/")
+        let normalizedTypePair = normalizedType.componentsSeparatedByString("/")
+        if messageTypePair.count == 2 && normalizedTypePair.count == 2
+            && messageTypePair[0] == normalizedTypePair[0] && normalizedTypePair[1] == "*" {
+            return true
+        }
+        return false
+    }
+    
+    
+    private class func normalizeType (type: String) -> String {
+        switch type {
+        case "urlencoded":
+            return "application/x-www-form-urlencoded"
+        case "multipart":
+            return "multipart/*"
+        case "json":
+            return "application/json"
+            // TODO: +json?
+//            if (type[0] === '+') {
+//                // "+json" -> "*/*+json" expando
+//                type = '*/*' + type
+//            }
+        default:
+            return type
+        }
+    }
+    
+    
+    private static var rawTypes = [
+        "text/plain": ["txt","text","conf","def","list","log","in","ini"],
+        "text/html": ["html", "htm"],
+        "text/css": ["css"],
+        "text/csv": ["csv"],
+        "text/xml": [],
+        "text/javascript": [],
+        "text/markdown": [],
+        "text/x-markdown": ["markdown","md","mkd"],
+        
+        "application/json": ["json","map"],
+        "application/x-www-form-urlencoded": [],
+        "application/xml": ["xml","xsl","xsd"],
+        "application/javascript": ["js"],
+        
+        "image/bmp": ["bmp"],
+        "image/png": ["png"],
+        "image/gif": ["gif"],
+        "image/jpeg": ["jpeg","jpg","jpe"],
+
+    ]
+    
+
+
+}
